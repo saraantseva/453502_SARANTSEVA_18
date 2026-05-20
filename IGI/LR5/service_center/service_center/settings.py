a6b5e419bf3c39857ac2ee7e57a3326ad7e58cb3
porphyriacus <lol@gmail.com>
2026-05-20 23:20:19 +0300

    add dockerfile docker-compose

--- IGI/LR5/service_center/service_center/settings.py
+++ IGI/LR5/service_center/service_center/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,21 +86,6 @@ LOG_TO_FILE = True
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-# База данных по умолчанию (для локальной разработки)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# Если есть переменная DATABASE_URL (на сервере) — используем PostgreSQL
-database_url = os.environ.get('DATABASE_URL')
-if database_url:
-    DATABASES['default'] = dj_database_url.config(
-        default=database_url,
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
 
 
 USE_TZ = True
@@ -143,3 +130,21 @@ LOGOUT_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# База данных по умолчанию (для локальной разработки)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# Если есть переменная DATABASE_URL (на сервере) — используем PostgreSQL
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
